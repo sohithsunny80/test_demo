9a0f3ba16d2bcb0c1f09e229ab82da69c524863a
sohithsunny80 <100835615+sohithsunny80@users.noreply.github.com>
2022-03-03 16:11:35 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,10 @@
 public class HelloWorld{
 public static void main(String[] args){
 {
-  System.out.println("hello sunny");
+  System.out.println("hello sohith");
+
+    System.out.println("hello sunny");
+
 }
 }
 }
